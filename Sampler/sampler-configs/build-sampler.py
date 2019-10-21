924c89a7b1d28cc4f50d4d588516d43763dfd46e
Kareem <bkmarzouk@outlook.com>
2019-10-21 13:03:09 +0100

    configuration files

---
+++ Sampler/sampler-configs/build-sampler.py
@@ -0,0 +1,187 @@
+""" Constructs sampler files for MapReduce programme"""
+
+import sys
+import os
+import shutil
+import importlib
+
+""" FLAG IF TESTING: OVERRIDES OS.ERRORS """
+Testing = True
+
+# Load user arguments
+user_args = sys.argv
+assert len(user_args) == 2
+
+# Get configuration module of interest
+config_module = user_args[1]
+
+# Import as module based on cmd line arg
+config = importlib.import_module(config_module[:-3])
+
+# Get paths from config
+pytpath = config.pytpath; sys.path.append(pytpath)
+import PyTransSetup as PySet; PySet.pathSet()
+saveloc = config.saveloc
+generator_name = os.path.join(saveloc, "generator.py")
+
+# Check that module is installed
+sampler = config.sampler
+try:
+    PyT = importlib.import_module(sampler["PyTransportModule"])
+except ImportError:
+    raise ImportError, "Failed to import PyTransport module: {}, check installation.".format(
+        sampler["PyTransportModule"]
+    )
+
+print PyT.nP()
+
+# Get required modules for sample generator
+modules = config.required_modules
+
+# Load file ICs and model parameters
+field_positions  = config.field_positions
+field_velocities = config.field_velocities
+parameter_values = config.parameter_values
+
+# Get installation number for cross validation
+nF = PyT.nF()
+nV = PyT.nF()
+nP = PyT.nP()
+
+# Build set of fiducial values for fields, velocities and parameters
+fiducial_fvals = [None for i in range(nF)]; cf = 0
+fiducial_vvals = [None for i in range(nV)]; cv = 0
+fiducial_pvals = [None for i in range(nP)]; cp = 0
+
+# Read each field value: if "ALL" is found, replace fiducial values with method prescribed in confiuration
+for p in field_positions:
+    if p["FieldNumber"] == "ALL":
+        fiducial_fvals = [p["Command"] for i in range(nF)]
+
+# Now reiterate over items: If anything but "ALL" is found, replace the fiducial method for the field
+for p in field_positions:
+    f_num = p["FieldNumber"]
+    if f_num != "ALL":
+        fiducial_fvals[f_num] = p["Command"]
+assert None not in fiducial_fvals
+
+
+# Repeat method for velocities
+for v in field_velocities:
+    if v["FieldNumber"] == "ALL":
+        fiducial_vvals = [v["Command"] for i in range(nV)]
+for v in field_velocities:
+    f_num = v["FieldNumber"]
+    if f_num != "ALL":
+        fiducial_vvals[f_num] = v["Command"]
+assert None not in fiducial_vvals
+
+
+# Repeat method for paramaters
+for p in parameter_values:
+    if p["ParameterNumber"] == "ALL":
+        fiducial_pvals = [p["Command"] for i in range(nP)]
+for p in parameter_values:
+    p_num = p["ParameterNumber"]
+    if p_num != "ALL":
+        fiducial_pvals[p_num] = p["Command"]
+assert None not in fiducial_pvals
+
+
+# We construct strings for generator files: We ned the correct formatting to be returned by the generator
+f_string = "["; fc = 1
+v_string = "["; vc = 1
+p_string = "["; pc = 1
+
+
+for f in fiducial_fvals:
+    if type(f) is not str:
+        f_string += str(f)
+    else: f_string += f
+    if fc != nF:
+        fc+=1
+        f_string += ","
+    else: f_string += "]"
+
+
+for v in fiducial_vvals:
+    if type(v) is not str:
+        v_string += str(v)
+    elif v == "SlowRoll": v_string+="'{}'".format(v)
+    else: v_string += v
+    if vc != nV:
+        vc+=1
+        v_string += ","
+    else: v_string += "]"
+
+
+for p in fiducial_pvals:
+    if type(p) is not str:
+        p_string += str(p)
+    else: p_string += p
+    if pc != nP:
+        pc+=1
+        p_string += ","
+    else: p_string += "]"
+
+
+# Setup import statements for writing process
+import_lines   = ["import {}\n".format(m) for m in modules]
+function_lines = [
+    "def gen_sample(n):\n",
+    "\tfvalues = {}\n".format(f_string),
+    "\tvvalues = {}\n".format(v_string),
+    "\tpvalues = {}\n".format(p_string),
+    "\treturn n, fvalues, vvalues, pvalues\n"
+]
+
+
+# If not in testing mode: Build save directory
+if not Testing:
+    assert not os.path.exists(saveloc), "Directory already exists! {}".format(saveloc)
+else:
+    try: os.makedirs(saveloc)
+    except OSError: pass
+
+
+# Define file for sample generator
+gfile = open(os.path.join(saveloc, "SampleGenerator.py"), "w")
+
+
+# With file: Write all lines
+with gfile:
+    print "-- Writing sample generator"
+    for line in import_lines + function_lines:
+        gfile.writelines(line)
+
+
+# Locate pre-written sampler files: e.g. mapreduce, etc
+sampler_file_dir = os.path.abspath(os.path.join(
+    os.getcwd(), "..", "samplerfiles"
+))
+sampler_files = [
+    #"MapReduceV2.py", "ObservableScriptsV2.py", "taskhandlerV2.py", "writerV2.py", "realization.py"
+    "MapReduceV2.py"
+]
+
+
+# Copy appropriate files
+for item in sampler_files:
+    print "-- Copying {}".format(item)
+    shutil.copyfile(
+        os.path.join(sampler_file_dir, item), os.path.join(saveloc, item)
+    )
+shutil.copyfile(os.path.abspath(os.path.join(os.getcwd(), config_module)),
+                os.path.join(saveloc, "config.py"))
+
+# Construct ensemble of
+build_dirs = ["samples", "outputs"]
+if config.computations['2pf'] is True: build_dirs.append("2pf")
+if config.computations['3pf'] is True: build_dirs.append("3pf")
+if config.computations['Mij'] is True: build_dirs.append("Mij")
+
+for item in build_dirs:
+    try:
+        os.makedirs(os.path.join(saveloc, item))
+    except:
+        if Testing is True: pass
---
+++ Sampler/sampler-configs/dquad_example.py
@@ -0,0 +1,123 @@
+"""
+
+To build a sampler, we are required to write a configuration file as follows
+
+"""
+
+# Standard preamble of modules
+import numpy as np
+import os
+import sys
+
+
+
+""" Path Configuration | AUTOMATIC """
+
+
+
+# Get python path: This should be automatic assuming directory structure has not been altered
+pytpath = (
+    os.path.join(
+        os.getcwd(), "..", "PyTransport", "PyTransport"
+    )
+)
+if not os.path.exists(pytpath):
+    pytpath = os.path.abspath(
+        os.path.join(
+            os.getcwd(), "..", "..", "PyTransport", "PyTransport"
+        )
+    ); assert os.path.exists(pytpath), "PyTransport location not found: {}".format(pytpath)
+
+savedir = os.path.abspath(
+    os.path.join(
+        os.getcwd(), "..", "Samplers",
+    )
+)
+if not os.path.exists(savedir):
+    savedir = os.path.abspath(
+        os.path.join(
+            os.getcwd(), "..", "..", "Samplers"
+        )
+    ); assert os.path.exists(savedir), "Save location not found: {}".format(savedir)
+
+
+
+""" Computation Configuration | CHANGE ME """
+
+
+sampler_name = "dquad_example"
+saveloc = os.path.join(savedir, sampler_name)
+
+
+sampler = { #
+    "PyTransportModule": "PyTrans2Quad", # Installed module, e.g. PyTrans2Quad
+    "NSamples": 200,                    # Compute N samples that support minimum number of efolds (see accept crit.)
+    "ExitTime": 55,                      # Assign number of efoldings before end of inflation that scales exit horizon
+    "SubEvolution": 6,                   # Duration (efoldings) of sub-horizon evolution
+    "tols": [1e-8, 1e-8]                 # Integration tolerance
+}
+
+
+computations = {
+    "2pf": True, # Compute 2-point function observables
+    "3pf": True, # Compute 3-point function observables
+    "Mij": True  # Evaluate Mass-Matrix Eigenvalues @ ExitTime, (indirectly) required if testing adiabatic limit
+}
+
+
+which3pf = [ # Assign sub-dictionaries specifying configuration name, latex commands and Ferg. Shell. params
+    {"config_name": "eq", "latex": "f_{NL}^{eq}", "alpha": 1./3., "beta": 1./3.}
+]
+
+
+accept_criteria = { # Accepts trajectories subject to...
+    "MinimumEfolds": 60,       # Required: Minimum amount of inflation for successful sampler
+    "TestAdiabaticLimit": True # Not Required: Infers adiabatic limit from background evolution
+}
+
+
+end_inflation = { # End inflation under slow-roll violation. If false, uses field conditions to end realisation
+    "Canonical": True,    # \epsilon > 1
+}
+
+
+end_conditions = [ # If non-canonical end, specify field relevant field values
+    None
+]
+
+
+
+""" Set Sampling Strategy, i.e. priors on initial conditions and parameters | CHANGE ME """
+
+
+
+required_modules = [ # Specify modules required for sampling strategy
+    "numpy", "scipy"
+]
+
+field_positions = [ # Set initial field-space positions
+    {"FieldNumber": "ALL",
+     "Command": "numpy.random.normal(-20, 20)"
+    },
+    {"FieldNumber": 0,
+     "Command": "numpy.random.uniform(-10, 10)"
+    }
+]
+
+field_velocities = [
+    {"FieldNumber": "ALL",
+     "Command": "SlowRoll"
+    }
+]
+
+parameter_values = [ # If None is given for latex, parameter will *not* be logged
+    {"ParameterNumber": 0,
+     "Command": 1e-3,
+     "LaTeX": "m_\chi"
+    },
+
+    {"ParameterNumber": 1,
+     "Command": "numpy.exp(numpy.random.uniform(-2, -5))",
+     "LaTeX": "m_\phi"
+    }
+]
