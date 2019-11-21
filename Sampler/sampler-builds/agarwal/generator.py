5458bff2790337a9e2d0b1d93759128ba3f5b31b
Kareem <bkmarzouk@outlook.com>
2019-11-21 14:09:57 +0000

    Removed agarwal build directory

--- Sampler/sampler-builds/agarwal/generator.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-def gen_sample(n):
-	fvalues = [0.9,1.0,1.0,1.0,1.0,1.0]
-	vvalues = [0,0,0,0,0,0]
-	pvalues = [0.01,0.001,np.random.uniform(0, 1),0.1,np.random.normal(0, 1e-12),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1),np.random.normal(0, 1)]
-	return n, fvalues, vvalues, pvalues
